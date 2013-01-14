a0182c31a48ac0c5190db398b6d20945d831139b
Fabio Rehm <fgrehm@gmail.com>
2013-01-14 20:48:29 -0200

    Setup extension namespace

--- lib/middleman-blog-drafts.rb
+++ lib/middleman-blog-drafts.rb
@@ -1,43 +1,44 @@
-# Require core library
 require "middleman-core"
 
-# Extension namespace
-module MyExtension
-  class << self
 
-    # Called when user `activate`s your extension
-    def registered(app, options={})
-      # Setup extension-specific config
-      app.set :config_variable, false
+module Middleman
+  module Blog
+    module Drafts
+      class << self
+        # Called when user `activate`s your extension
+        def registered(app, options={})
+          # Setup extension-specific config
+          app.set :config_variable, false
 
-      # Include class methods
-      # app.extend ClassMethods
+          # Include class methods
+          # app.extend ClassMethods
 
-      # Include instance methods
-      # app.send :include, InstanceMethods
+          # Include instance methods
+          # app.send :include, InstanceMethods
 
-      app.after_configuration do
-        # Do something
+          app.after_configuration do
+            # Do something
 
-        # config_variable is now either the default or the user's
-        # setting from config.rb
+            # config_variable is now either the default or the user's
+            # setting from config.rb
+          end
+        end
+        alias :included :registered
       end
+
+      # module ClassMethods
+      #   def a_class_method
+      #   end
+      # end
+
+      # module InstanceMethods
+      #   def an_instance_method
+      #   end
+      # end
+
     end
-    alias :included :registered
   end
-
-  # module ClassMethods
-  #   def a_class_method
-  #   end
-  # end
-
-  # module InstanceMethods
-  #   def an_instance_method
-  #   end
-  # end
-
 end
-
 
 # Register extensions which can be activated
 # Make sure we have the version of Middleman we expect
